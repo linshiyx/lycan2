766af4b6b734fec41586262054162fb227cf6c77
Antelope <421829514@qq.com>
2016-09-29 16:43:37 +0800

    调整数据长度限制

--- app/models.py
+++ app/models.py
@@ -3,20 +3,20 @@ from django.db import models
 
 # Create your models here.
 class Room(models.Model):
-    room_id = models.CharField(max_length=20, db_index=True)
+    room_id = models.CharField(max_length=30, db_index=True)
     has_started = models.BooleanField(default=False)
     game_round = models.IntegerField(default=0)
-    users = models.CharField(max_length=2000)
-    valentine = models.CharField(max_length=50, default='[]')
-    guarded = models.CharField(max_length=60, default='{"now":"", "last":""}')
-    lycan = models.CharField(max_length=200, default='{"lycan_choice": {}}')
-    poison = models.CharField(max_length=200, default='{"poison": 3}')
-    badge = models.CharField(max_length=20, default='')
+    users = models.CharField(max_length=10000)
+    valentine = models.CharField(max_length=100, default='[]')
+    guarded = models.CharField(max_length=100, default='{"now":"", "last":""}')
+    lycan = models.CharField(max_length=400, default='{"lycan_choice": {}}')
+    poison = models.CharField(max_length=400, default='{"poison": 3}')
+    badge = models.CharField(max_length=30, default='')
     can_talk = models.BooleanField(default=True)
-    talk_list = models.CharField(max_length=500, default='[]')
-    finish_talk = models.CharField(max_length=500, default='[]')
-    vote_badge = models.CharField(max_length=500, default='{}')
-    vote_dead = models.CharField(max_length=500, default='{}')
+    talk_list = models.CharField(max_length=1000, default='[]')
+    finish_talk = models.CharField(max_length=1000, default='[]')
+    vote_badge = models.CharField(max_length=1000, default='{}')
+    vote_dead = models.CharField(max_length=1000, default='{}')
 
     def __unicode__(self):
         return self.room_id
